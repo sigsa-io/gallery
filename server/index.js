db6916648cca1b5766afef373579495c619e035a
Parteek S. Sandhu <ParteekSSandhu@MacBook-Pro-5.local>
2019-07-03 17:31:20 -0700

    Setup development environment & serve static files

--- server/index.js
+++ server/index.js
@@ -2,6 +2,8 @@ const express = require('express')
 const app = express()
 const port = 3000
 
-app.get('/', (req, res) => res.send('Hello World!'))
+app.use(express.static(__dirname + '/../client/dist'));
+
+app.get('/', (req, res) => res.send('hit /'))
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
